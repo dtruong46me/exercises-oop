45a877dfed5604d9fb72f90b1e99baade4b86a0b
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2023-03-22 17:03:37 +0700

    main

---
+++ Problem1/Officier.java
@@ -0,0 +1,47 @@
+package Problem1;
+
+public class Officier {
+    protected String name;
+    protected int age;
+    protected String gender;
+    protected String address;
+
+    public Officier(String name, int age, String gender, String address) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    // ---------------------------------------------//
+    public String GetName(){
+        return name;
+    }
+    public void SetName(String name) {
+        this.name = name;
+    }
+
+    // ---------------------------------------------//
+    public int GetAge() {
+        return age;
+    }
+    public void SetAge(int age) {
+        this.age = age;
+    }
+
+    // ---------------------------------------------//
+    public String GetGender() {
+        return gender;
+    }
+    public void SetGender(String gender) {
+        this.gender = gender;
+    }
+
+    // ---------------------------------------------//
+    public String GetAddress() {
+        return address;
+    }
+    public void SetAddress(String address) {
+        this.address = address;
+    }
+}
